b39c36582f100bdd8da499b66f3741429830d1c9
Jorge Duarte <sadujorge@gmail.com>
2024-04-24 22:36:38 -0700

    Initial styling completed

--- client/src/components/RadioButtonGroup.jsx
+++ client/src/components/RadioButtonGroup.jsx
@@ -1,20 +1,28 @@
 // eslint-disable-next-line
 function RadioButtonGroup({ label, name, options, value, onChange }) {
-    const stringValue = String(value);  
+    const stringValue = String(value);
     return (
         <div className="mb-3 text-center">
-            <label>{label}</label>
-            {/* eslint-disable-next-line */}
-            {options.map(option => (
-                <label key={option}>
-                    <input
-                        type="radio"
-                        name={name}
-                        value={option}
-                        checked={stringValue === option}
-                        onChange={onChange} /> {option === "true" ? "Yes" : option === "false" ? "No" : "N/A"}
-                </label>
-            ))}
+            <label className="form-label d-block">{label}</label>
+            <div className="d-flex justify-content-center">
+                {/* eslint-disable-next-line */}
+                {options.map((option, index) => (
+                    <div key={index} className="form-check form-check-inline">
+                        <input
+                            className="form-check-input"
+                            type="radio"
+                            name={name}
+                            id={`${name}-${option}`}
+                            value={option}
+                            checked={stringValue === option}
+                            onChange={onChange}
+                        />
+                        <label className="form-check-label" htmlFor={`${name}-${option}`}>
+                            {option === "true" ? "Yes" : option === "false" ? "No" : "N/A"}
+                        </label>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
